4885af5a4f8f71f26c31a1e620b9d55b2e971e8b
zhu <zhuy1@126.com>
2020-11-05 11:57:06 +0800

    添加项目文件。

---
+++ DesignPatternDemo/SingletonPatternDemo/HungrySingletonPattern.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternDemo
+{
+    public class HungrySingletonPattern
+    {
+        public static HungrySingletonPattern instance = new HungrySingletonPattern();
+
+        private HungrySingletonPattern() 
+        { }
+
+        public static HungrySingletonPattern GetInstance()
+        {
+            return instance;
+        }
+    }
+}
